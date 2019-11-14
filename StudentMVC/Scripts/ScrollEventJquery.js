d335e60de4f7737c99dd9d68f552b323dd10cbfd
Alumno <Alumno@M14-CI>
2019-11-14 11:52:54 -0600

    Scroll Event baner hide

---
+++ StudentMVC/Scripts/ScrollEventJquery.js
@@ -0,0 +1,12 @@
+﻿var lastScrollTop = 0;
+$(window).scroll(function (event) {
+    var st = $(this).scrollTop();
+    if (st > lastScrollTop) {
+        // downscroll code
+        document.getElementById("nav").style.display = "none"
+    } else {
+        // upscroll code
+        document.getElementById("nav").style.display = "inline-block"
+    }
+    lastScrollTop = st;
+});
