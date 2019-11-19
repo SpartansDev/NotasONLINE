5d52e237ec71f65a09498c901b907f165442e0a3
Bryan J. Campos <Bryan J. Campos@LAPTOP-AG12FGOC>
2019-11-19 16:41:06 -0600

    quitando codigo basura

--- DAL/ConexionBD.cs
+++ DAL/ConexionBD.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data;
-using System.Data.SqlClient;
+﻿using System.Data.SqlClient;
 namespace DAL
 {
     public class ConexionBD
     {
+        #region conexion a la base de datos
         private static string cadena = @"Data Source=.;Initial Catalog=NotasOnline;Integrated Security=True";
         public static SqlConnection Conectar()
         {
             return new SqlConnection(cadena);
         }
+        #endregion
     }
 }
