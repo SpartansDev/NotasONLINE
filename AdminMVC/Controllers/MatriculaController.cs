32f11af3385a9a1224f20e2a8c2ece8a71e8bae0
Jose Guillen <nfsrotari15@gmail.com>
2019-11-20 12:54:14 -0600

    ordenado

--- AdminMVC/Controllers/MatriculaController.cs
+++ AdminMVC/Controllers/MatriculaController.cs
@@ -12,14 +12,19 @@ namespace AdminMVC.Controllers
     {
         MatriculaBL bl = new MatriculaBL();
         // GET: Matricula
+        #region Index
         public ActionResult Index()
         {
             return View();
         }
+        #endregion
+        #region Matricula
         public ActionResult Matricula()
         {
             return View();
         }
+        #endregion
+        #region Agregar
 
         [Authorize]
         [HttpPost]
@@ -27,41 +32,53 @@ namespace AdminMVC.Controllers
         {
             return Json(bl.Agregar(pMatricula), JsonRequestBehavior.AllowGet);
         }
+        #endregion
+        #region Modificar
         [Authorize]
         [HttpPost]
         public JsonResult Modificar(Matricula pMatricula)
         {
             return Json(bl.Modificar(pMatricula), JsonRequestBehavior.AllowGet);
         }
-
+        #endregion
+        #region Verificar Matricula
         [HttpPost]
         public JsonResult verificar(Matricula pMatricula)
         {
             return Json(bl.verificar(pMatricula), JsonRequestBehavior.AllowGet);
         }
+        #endregion
+        #region Obtener Por Id
         [Authorize]
         [HttpGet]
         public JsonResult ObtenerPorId(Int64 pId)
         {
             return Json(MatriculaBL.ObtenerPorId(pId), JsonRequestBehavior.AllowGet);
         }
+        #endregion
+        #region Mostrar
         [Authorize]
         [HttpGet]
         public JsonResult Mostrar()
         {
             return Json(bl.Mostrar(),JsonRequestBehavior.AllowGet);
         }
+        #endregion
+        #region Buscar Por Codigo
         [Authorize]
         [HttpGet]
         public JsonResult buscarPorCodigo(string pText)
         {
             return Json(bl.buscarCodigo(pText), JsonRequestBehavior.AllowGet);
         }
+        #endregion
+        #region Alumnos
         [Authorize]
         [HttpGet]
         public JsonResult misAlumnos(Int64 pId)
         {
             return Json(bl.misAlumnos(pId), JsonRequestBehavior.AllowGet);
         }
+        #endregion
     }
 }
