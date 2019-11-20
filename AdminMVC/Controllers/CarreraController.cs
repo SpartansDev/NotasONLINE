e32b151ccc86cc57c454444cab30596b007c0340
Jose Guillen <nfsrotari15@gmail.com>
2019-11-20 10:57:50 -0600

    Indentacion Carrera

--- AdminMVC/Controllers/CarreraController.cs
+++ AdminMVC/Controllers/CarreraController.cs
@@ -12,34 +12,44 @@ namespace AdminMVC.Controllers
     {
         CarreraBL bl = new CarreraBL();
         // GET: Carrera
+        #region Index
         public ActionResult Index()
         {
             return View();
         }
+        #endregion
+        #region Metodo Agregar Carrera
         [Authorize]
         [HttpPost]
         public JsonResult Agregar(Carrera pCarrera)
         {
             return Json(bl.Agregar(pCarrera), JsonRequestBehavior.AllowGet);
         }
+        #endregion
+        #region Metodo Modificar
         [Authorize]
         [HttpPost]
         public JsonResult Modificar(Carrera pcarrera)
         {
             return Json(bl.Modificar(pcarrera), JsonRequestBehavior.AllowGet);
         }
+        #endregion
+        #region Metodo Mostrar
         [Authorize]
         [HttpGet]
         public JsonResult mostrar()
         {
             return Json(bl.Mostrar(), JsonRequestBehavior.AllowGet);
         }
+        #endregion
+        #region obtener por ID
         [Authorize]
         [HttpGet]
         public JsonResult ObtenerPorId(int pId)
         {
             return Json(CarreraBL.ObtenerPorId(pId), JsonRequestBehavior.AllowGet);
         }
+        #endregion
 
     }
 }
