c30d5d175d33761daee13f0b62ad2ad7ed8643d2
Jose Guillen <nfsrotari15@gmail.com>
2019-11-20 13:10:56 -0600

    listo

--- AdminMVC/Controllers/ProfesorController.cs
+++ AdminMVC/Controllers/ProfesorController.cs
@@ -12,10 +12,13 @@ namespace AdminMVC.Controllers
     {
         ProfesorBL bl = new ProfesorBL();
         // GET: Profesor
+        #region Index
         public ActionResult Index()
         {
             return View();
         }
+        #endregion
+        #region Perfil
         public ActionResult Perfil()
         {
            // return View();
@@ -31,36 +34,46 @@ namespace AdminMVC.Controllers
                 return Redirect("/Home/Index");
             }
         }
+        #endregion
+        #region Agregar
         [Authorize]
         [HttpPost]
         public JsonResult Agregar(Profesor pp)
         {
             return Json(bl.Agregar(pp), JsonRequestBehavior.AllowGet);
         }
+        #endregion
+        #region Modificar
         [Authorize]
         [HttpPost]
         public JsonResult Modificar(Profesor pp)
         {
             return Json(bl.Modificar(pp), JsonRequestBehavior.AllowGet);
         }
+        #endregion
+        #region Mostrar
         [Authorize]
         [HttpGet]
         public JsonResult Mostrar()
         {
             return Json(bl.Mostrar(), JsonRequestBehavior.AllowGet);
         }
-
+        #endregion
+        #region Obtener Por Id
         [Authorize]
         [HttpGet]
         public JsonResult ObtenerPorId(Int64 pId)
         {
             return Json(ProfesorBL.ObtenerPorId(pId), JsonRequestBehavior.AllowGet);
         }
+        #endregion
+        #region Existe Profesor
         [Authorize]
         [HttpPost]
         public JsonResult ExisteProfesor(Profesor pProfesor)
         {
             return Json(bl.ProfesorNoExiste(pProfesor), JsonRequestBehavior.AllowGet);
         }
+        #endregion
     }
 }
