2163315b64273f40e264f49a4946432bb553747a
Alumno <Alumno@M14-CI>
2019-11-27 11:13:42 -0600

    Modulos script SQL

---
+++ AdminMVC/BaseDatos/Módulos.sql
@@ -0,0 +1,156 @@
+Use NotasOnline
+
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Diseño de Sistemas Informáticos', 1, 8)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Construcción de Bases de Datos', 1, 6)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Construcción de Aplicaciones de Escritorio', 1, 7)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Aplicación de Inglés Básico en Áreas Técnicas de Ciclo I', 1, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Construcción de Aplicaciones Web', 1, 6)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Configuración de Servidores Web', 1, 5)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Programación de Aplicaciones Informáticas en Lenguaje PHP', 1, 6)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Gestión de Planes de Negocio de Tecnologías de la Información', 1, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Aplicación de Inglés Básico en Áreas Técnicas de Ciclo II', 1, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Programación de Aplicaciones Informáticas en Lenguaje ASP.NET y HTML', 1, 6)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Programación de Aplicaciones para Móviles', 1, 6)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Testeo de Software', 1, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Formulación de de Proyectos de Innovación del Área de Desarrollo de Software', 1, 5)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Conversación en Inglés Aplicada a Áreas Técnicas de Ciclo III', 1, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Administración de Proyectos de Desarrollo de Software', 1, 5)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Implementación de Sistemas Informáticos', 1, 7)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Soporte Técnico a Sistemas Informáticos', 1, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Conversación en Inglés Aplicada a Áreas Técnicas de Ciclo IV', 1, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Ejecución de Proyectos de Innovación del Área de Desarrollo de Software', 1, 5)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Construcción de Instalaciones Eléctricas Residenciales y Comerciales', 2, 7)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Mantenimiento de Máquinas Rotativas', 2, 5)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Mantenimiento de Sistemas Hidronehumáticos de Equipos de Bombeo', 2, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Aplicación de Inglés Básico I', 2, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Construcción de Instalaciones Eléctricas Industriales', 2, 5)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Construcción de Líneas Secundarias en Baja y Media Tensión', 2, 5)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Construcción de Subestaciones de Distrubución Eléctrica', 2, 6)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Aplicación de Inglés Básico II', 2, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Construcción de Sistemas de Control Automático de Procesos Industriales y Adquisión de Señales', 2, 5)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Mantenimiento de Sistemas de Refrigeración y Aire Acondicionado', 2, 5)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Implementación de Sistemas SCADA para Procesos Industriales', 2, 8)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Mantenimiento de Sistemas de Control Automático de Procesos Industriales', 2, 5)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Generación de Energía Eléctrica, Fotovoltáica, Biogas y Biomasa', 2, 5)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Conversación en Inglés Aplicada al Área Eléctrica I', 2, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Formulación de Proyectos de Innovación del Área de Electricidad', 2, 3)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Gestión de Plan de Negocios', 2, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Supervisión de Ejecución de Programa de Mantenimiento', 2, 5)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Implementación de Medidas de Eficiencia Energética', 2, 5)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Ejecución de Proyectos de Innovación del Ärea de Electricidad', 2, 5)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Conversación en Inglés Aplicada al Ärea Eléctrica II', 2, 8)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Servicio y Atención al Cliente', 3, 3)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Elaboración de Mezcla de Marketing', 3, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Venta de Productos y Servicios NM 00', 3, 3)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Animación de Eventos Promocionales', 3, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Aplicación de Inglés Básico 1', 3, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Gestión de Cobros y Telemercadeo', 3, 3)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Desarrollo de Mercado Virtual', 3, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Diseño de Campaña Publicitaria', 3, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Gestión y Comercialización de Productos y Servicios', 3, 3)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Aplicación de Inglés 2', 3, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Investigación de Mercado', 3, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Elaboración de Planes de Mercadeo', 3, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Elaboración de Planes de Venta', 3, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Proyecto de Innovación sobre Venta de Productos o Servicios', 3, 2)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Conversación en Inglés Aplicado al Servicio y Atención al Cliente', 3, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Supervisión de Ventas', 3, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Supervisión de Personal', 3, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Gestión de Planes de Negocios', 3, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Proyecto de Ingestigación sobre E-COMMERCE', 3, 2)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Conversación de Inglés Aplicado a la Comercialización de Productos', 3, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Elaboración de Inventarios Turísticos', 4, 5)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Gestión de Establecimientos de Alojamiento', 4, 5)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Gestión de Cocinas y Bodegas', 4, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Promoción de Atractivos Turísticos', 4, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Aplicacion de Inglés Básico 1', 4, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Gestión de Elementos Culturales', 4, 6)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Diseño de Productos Turísticos', 4, 5)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Gestión e Intermediación Turística', 4, 5)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Gestión de Eventos y Banquetes', 4, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Aplicación de Inglés Básico 2', 4, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Diseño de Paquetes Turísticos', 4, 8)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Guía y Asistencia de Grupos Turísticos', 4, 8)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Formulación de Proyectos de Innovación del Área de Turismo', 4, 4)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Conversación en Inglés Aplicado a los Servicios Turísticos', 4, 5)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Gestión de Planes de Negocios Turísticos', 4, 8)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Gestión de Desarrollo de Destinos Turísticos Sostenibles', 4, 6)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Ejecución de Proyecto de Innovación del Área de Turismo', 4, 5)
+Go
+Insert Into Modulos(NombreModulo, CarreraId, UV) Values('Conversación de Inglés Aplicado a la Industria Turística', 4, 5)
+Go
